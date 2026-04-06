f444b88c774d3c4065bf41b22df4568f5e2e11a8
lucawltrs <luca.wltrs@gmail.com>
2026-04-06 12:57:47 +0200

    -fix percentage

--- src/modules/4based/components/ChatMessageList.tsx
+++ src/modules/4based/components/ChatMessageList.tsx
@@ -137,7 +137,7 @@ export function ChatMessageList({
                               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                             </svg>
                           )}
-                          ${(price * (1 - 0.21) / 100).toFixed(2)}
+                          ${(price / 1.21 / 100).toFixed(2)}
                         </span>
                       )}
                       {totalCount > 1 && (
